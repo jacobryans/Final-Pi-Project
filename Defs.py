fdffc062129ccc932dda3429854204e4d97d554f
Zero <Jrr043@latech.edu>
2017-05-03 19:28:45 -0500

    Defs debugging

--- Defs.py
+++ Defs.py
@@ -1,40 +1,45 @@
+class ItemDefs:
+    global items
+
+    items = {
+        'testitem' : {
+            'type' : 'usable',
+            'key' : True,
+            'desc' : 'A Test Item'
+            },
+        }
+
+    def __init__(self):
+        self.items = items
+
+
+class RoomDefs:
+    global rooms
+    
+    rooms = {
+        'testroom' : {
+            'locked' : False,
+            'background' : 'pic1',
+            'items' : items['testitem'],
+            'desc' : 'A Test Room'
+            },
+        }
+
+    def __init__(self):
+        self.rooms = rooms
+
+
 class MapDefs:
     global areas
     
     areas = {
-        "Old Town" : {
-                "rooms" : [ rooms["testroom"],  ],
-                "loot" : [  ]
+        'Old Town' : {
+                'rooms' : [ rooms['testroom'],  ],
+                'loot' : [  ]
         },
     }
     
     def __init__(self):
         self.areas = areas
 
-class ItemDefs:
-    global items
-
-    items = {
-        "testitem" : {
-            "type" : usable,
-            "key" : True,
-            "desc" : "A Test Item"
-            },
-        }
-
-    def __init__(self):
-        self.items = items
         
-class RoomDefs:
-    global rooms
-
-    rooms = {
-        "testroom" : {
-            "locked" : False,
-            "background" : "pic1",
-            "items" : items["testitem"],
-            "desc" : "A Test Room"
-            }
-            
-    def __init__(self):
-        self.rooms = rooms
