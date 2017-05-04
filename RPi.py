0b1d98eec80f8131e415c24f7747632d9700c01a
DESKTOP-ABULPVI\rlebe <rlebell98@gmail.com>
2017-05-03 19:21:55 -0500

    GPIO inputs

--- RPi.py
+++ RPi.py
@@ -1,6 +1,31 @@
 from Engine import *
 from Game import *
+import RPi.GPIO as GPIO
+from random import randint
+import pygame
+#declares what switches are used for inputs
+switches = [ 23, 18, 24, 25 ]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(switches, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-class PinDetection:
-    def PinStatus:
-        pass
+try:
+
+    while(True):
+        if(GPIO.input(switches[0]) == True):
+            print "north"
+            
+        elif(GPIO.input(switches[1]) == True):
+            print "south"
+            
+        elif(GPIO.input(switches[2]) == True):
+            print "east"
+            
+        elif(GPIO.input(switches[3]) == True):
+            print "west"
+            
+        else:
+            print "nothing detected.."
+        sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    
